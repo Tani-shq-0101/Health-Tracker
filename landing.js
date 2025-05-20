30b97fb8a1e427599a2fc0c732298871371db0c6
codegeek2002 <tanishqcse0101@gmail.com>
2025-05-21 04:52:31 +0530

    landing page js commit

---
+++ landing.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Health Tracking","Mental Health Support","Diet planning"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
